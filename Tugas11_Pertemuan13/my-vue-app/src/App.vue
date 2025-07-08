96627e04d38e8e846368adfcac095c7826b7f023
YesaAradyaPasha <yesaaradya11216@gmail.com>
2025-07-08 18:01:28 +0700

    first commit

---
+++ Tugas11_Pertemuan13/my-vue-app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="p-4 bg-gray-200 flex gap-4 font-semibold">
+    <router-link to="/" class="text-blue-600 hover:underline">Daftar User</router-link>
+    <router-link to="/create" class="text-blue-600 hover:underline">Tambah User</router-link>
+  </nav>
+  <main class="p-6">
+    <router-view />
+  </main>
+</template>
+
+<script setup>
+// Tidak perlu import router-link dan router-view
+</script>
